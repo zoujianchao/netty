42bf793316bec61fd8474513c2b212125a4c6488
zoujianchao <data_zjc@163.com>
2021-07-03 16:27:23 +0800

    add transferFrom copy file

--- src/main/java/com/zjc/nio/NIOFileChannel04.java
+++ src/main/java/com/zjc/nio/NIOFileChannel04.java
@@ -1,5 +1,9 @@
 package com.zjc.nio;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
 /**
  * @author : zoujianchao
  * @version : 1.0
@@ -7,7 +11,19 @@ package com.zjc.nio;
  * @description : 拷贝文件 用transferFrom
  */
 public class NIOFileChannel04 {
-    public static void main(String[] args) {
-        //增加NIO
+    public static void main(String[] args) throws Exception{
+        //创建相关流
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\data_\\Desktop\\a1.jpg");
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\data_\\Desktop\\a2.jpg");
+        //获取各流对应的fileChannel
+        FileChannel sourceCh = fileInputStream.getChannel();
+        FileChannel destCh = fileOutputStream.getChannel();
+        //使用transferFrom完成拷贝
+        destCh.transferFrom(sourceCh, 0, sourceCh.size());
+        //关闭相关的通道和流
+        sourceCh.close();
+        destCh.close();
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 }
